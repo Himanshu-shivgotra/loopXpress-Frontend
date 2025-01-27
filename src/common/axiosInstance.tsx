66694e636b59b90a7d1aceed0beb71310c989269
Himanshu Shivgotra <himanshushivgotra.com>
2025-01-27 18:09:54 +0530

    apexchart error resolved

--- src/common/axiosInstance.tsx
+++ src/common/axiosInstance.tsx
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   // baseURL: 'https://loop-xpress-backend.vercel.app', // Base URL of your backend
-  baseURL: 'http://localhost:5000',
+  // baseURL: 'http://localhost:5000',
+  baseURL: 'https://loopxpress-backend.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
