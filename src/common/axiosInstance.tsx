1eceaeae2f79b8e3730a69c34b02b388c737ac47
Himanshu Shivgotra <himanshushivgotra.com>
2025-02-14 17:52:15 +0530

    change axios backend path hosted

--- src/common/axiosInstance.tsx
+++ src/common/axiosInstance.tsx
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   // baseURL: 'https://loop-xpress-backend.vercel.app', // Base URL of your backend
-  baseURL: 'http://localhost:5000',
-  // baseURL: 'https://loopxpress-backend.onrender.com',
+  // baseURL: 'http://localhost:5000',
+  baseURL: 'https://loopxpress-backend.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
